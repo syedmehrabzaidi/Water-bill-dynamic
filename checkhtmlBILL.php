60f201bec166a2003de2f761ccc29e7ed20bc0cb
Mehrab Zaidi <45766880+syedmehrabzaidi@users.noreply.github.com>
2021-06-15 22:59:27 +0500

    work1

---
+++ checkhtmlBILL.php
@@ -0,0 +1,487 @@
+<head>
+  <meta charset="utf-8">
+
+</head>
+<style>
+    .main{
+      background: url('images/background.png')no-repeat;
+      position: absolute;
+      width: 11.07in; 
+      height: 15.17in;
+    }    
+      
+    .town{
+      position: absolute;   
+      top: 128px;
+      left: 206px;
+    }
+    .bill_type{
+      position: absolute; 
+      left: 406px;
+      top: 128px;
+    }
+    .name{
+      position: absolute;     
+      left: 25px;
+      top: 225px;
+    }
+    .address{
+      position: absolute;
+      left: 25px;
+      top: 245px;
+    }
+    .town_abbr{
+      position: absolute;
+      left: 535px;
+      top: 210px;
+    } 
+    .bill_per{
+      position: absolute;
+      left: 675px;
+      top: 190px;
+    }
+    .issue_date{
+      position: absolute;
+      left: 815px;
+      top: 190px;
+    }
+    .due_date{
+      position: absolute;
+      left: 945px;
+      top: 190px;
+    }
+    .water_curr{
+      position: absolute;
+      left: 695px;
+      top: 245px;
+    }
+   
+    .water_arr{
+      position: absolute;
+      left: 840px;
+      top: 245px;
+    }
+    .water_total{
+      position: absolute;
+      left: 965px;
+      top: 245px;
+    }
+    .sev_curr{
+      position: absolute;
+      left: 695px;
+      top: 275px;
+    }
+    .sev_arr{
+      position: absolute;
+      left: 840px;
+      top: 275px;
+    }
+    .sev_total{
+      position: absolute;
+      left: 965px;
+      top: 275px;
+    }
+    .cons_curr{
+      position: absolute;
+      left: 695px;
+      top: 300px;
+    }
+    .cons_arr{
+      position: absolute;
+      left: 840px;
+      top: 300px;
+    }
+    .cons_total{
+      position: absolute;
+      left: 965px;
+      top: 300px;
+    }.fire_curr{
+      position: absolute;
+      left: 695px;
+      top: 330px;
+    }
+    .fire_arr{
+      position: absolute;
+      left: 840px;
+      top: 330px;
+    }
+    .fire_total{
+      position: absolute;
+      left: 965px;
+      top: 330px;
+    }
+    .bank_total{
+      position: absolute;
+      left: 965px;
+      top: 355px;
+    }.payable_due_date{
+      position: absolute;
+      left: 965px;
+      top: 380px;
+    }.water_sur{
+      position: absolute;
+      left: 965px;
+      top: 405px;
+    }.payable_after_date{
+      position: absolute;
+      left: 965px;
+      top: 430px;
+    }
+     .Outstanding_Arrears{
+      position: absolute;
+      left: 600px;
+      top: 650px;
+
+    }
+    .Water{
+      position: absolute;
+      left: 770px;
+      top: 650px;
+
+    }
+    .Sewerage{
+      position: absolute;
+      left: 980px;
+      top: 650px;
+
+    }
+
+     .Conservancy{
+      position: absolute;
+      left: 600px;
+      top: 676px;
+
+    }
+    .Fire{
+      position: absolute;
+      left: 770px;
+      top: 676px;
+
+    }
+    .Total{
+      position: absolute;
+      left: 980px;
+      top: 676px;
+
+    }
+
+     .bill_per1{
+      position: absolute;
+      left: 675px;
+      top: 980px;
+    }
+    .issue_date1{
+      position: absolute;
+      left: 815px;
+      top: 980px;
+    }
+    .due_date1{
+      position: absolute;
+      left: 945px;
+      top: 980px;
+    }
+
+
+    .bank_water_curr{
+      position: absolute;
+      left: 695px;
+      top: 1038px;
+    }
+   
+    .bank_water_arr{
+      position: absolute;
+      left: 840px;
+      top: 1038px;
+    }
+    .bank_water_total{
+      position: absolute;
+      left: 965px;
+      top: 1038px;
+    }
+    .bank_sev_curr{
+      position: absolute;
+      left: 695px;
+      top: 1059px;
+    }
+    .bank_sev_arr{
+      position: absolute;
+      left: 840px;
+      top: 1059px;
+    }
+    .bank_sev_total{
+      position: absolute;
+      left: 965px;
+      top: 1059px;
+    }
+    
+
+
+
+    .bank_cons_curr{
+      position: absolute;
+      left: 695px;
+      top: 1088px;
+    }
+    .bank_cons_arr{
+      position: absolute;
+      left: 840px;
+      top: 1088px;
+    }
+    .bank_cons_total{
+      position: absolute;
+      left: 965px;
+      top: 1088px;
+    }
+    .bank_fire_curr{
+      position: absolute;
+      left: 695px;
+      top: 1115px;
+    }
+    .bank_fire_arr{
+      position: absolute;
+      left: 840px;
+      top: 1115px;
+    }
+    .bank_fire_total{
+      position: absolute;
+      left: 965px;
+      top: 1115px;
+    }
+    .bank_bank_total{
+      position: absolute;
+      left: 965px;
+      top: 1142px;
+    }
+
+    .bank_payable_due_date{
+      position: absolute;
+      left: 965px;
+      top: 1169px;
+    }.bank_water_sur{
+      position: absolute;
+      left: 965px;
+      top: 1086px;
+    }
+    .bank_water_sur{
+      position: absolute;
+      left: 965px;
+      top: 1200px;
+    }
+    .bank_payable_after_date{
+      position: absolute;
+      left: 965px;
+      top: 1224px;
+    }
+    .bank_town_abbr{
+    position: absolute;
+      left: 535px;
+      top: 990px;}
+
+    .bank_name{
+      position: absolute;     
+      left: 25px;
+      top: 1068px;
+    }
+    .bank_address{
+      position: absolute;
+      left: 25px;
+      top: 1089px;
+    }
+     .number1{
+      position: absolute;
+      left: 25px;
+      top: 1270px;
+    }
+     .number2{
+      position: absolute;
+      left: 135px;
+      top: 1270px;
+    }
+    th.heading-text {
+      font-size: 11px !important;
+      text-align: center;
+    }
+    td.table-text {
+      font-size: 10px !important;
+    }
+    .table_class {
+      position: absolute;
+      left: 41px;
+      top: 350px;
+      width: 36.5%;
+    }
+    table, th, td {
+      border: 1px solid black;
+    }
+
+</style>
+<body>
+<div class="main">
+  <div class="town">
+    Gardern East
+  </div>
+  <div class="bill_type">
+    Complete Bill
+  </div>
+  <div class="name">
+    Doulat Bai
+  </div>
+  <div class="address">
+    OFFICE NO # 31  I0th FLOOR
+  </div>
+  <div class="town_abbr">
+    SAD
+  </div>
+  <div class="bill_per">
+    May 2021
+  </div>
+  <div class="issue_date">
+    May 2021
+  </div>
+  <div class="due_date">
+    May 2021
+  </div>
+  <div class="water_curr">123</div>
+  <div class="water_arr">0</div>
+  <div class="water_total">255</div>
+  <div class="sev_curr">0</div>
+  <div class="sev_arr">0</div>
+  <div class="sev_total">0</div>
+  <div class="cons_curr">0</div>
+  <div class="cons_arr">0</div>
+  <div class="cons_total">0</div>
+  <div class="fire_curr">0</div>
+  <div class="fire_arr">0</div>
+  <div class="fire_total">0</div>
+  <div class="bank_total">8</div>
+  <div class="payable_due_date">8</div>
+  <div class="water_sur">8</div>
+  <div class="payable_after_date">8</div>
+
+  <div class="Outstanding_Arrears">101</div>
+  <div class="Water">102</div>
+  <div class="Sewerage">103</div>
+
+  <div class="Conservancy ">201</div>
+  <div class="Fire">202</div>
+  <div class="Total">203</div>
+
+  <div class="bill_per1"> May 2021  </div>
+  <div class="issue_date1"> May 2021  </div>
+  <div class="due_date1"> May 2021  </div>
+ 
+  <div class="bank_water_curr">11</div>
+  <div class="bank_water_arr">0</div>
+  <div class="bank_water_total">255</div>
+  <div class="bank_sev_curr">0</div>
+  <div class="bank_sev_arr">0</div>
+  <div class="bank_sev_total">0</div>
+  <div class="bank_cons_curr">0</div>
+  <div class="bank_cons_arr">0</div>
+  <div class="bank_cons_total">0</div>
+  <div class="bank_fire_curr">1</div>
+  <div class="bank_fire_arr">1</div>
+  <div class="bank_fire_total">1</div>
+  <div class="bank_bank_total">8</div>
+  <div class="bank_payable_after_date">8</div>
+  <div class="bank_water_sur">8</div>
+  <div class="bank_payable_due_date">8</div>
+  <div class="bank_town_abbr"> SAD </div>
+  <div class="bank_name"> Doulat Bai  </div>
+  <div class="bank_address">    OFFICE NO # 31  I0th FLOOR  </div>
+   <div class="number1">052021110</div>
+  <div class="number2">066011102800095</div>
+   
+ 
+  <table class="table_class">
+  <tbody>
+  <tr>
+    <th class="heading-text">Month</th>
+    <th class="heading-text">Account Billed</th>
+  <th class="heading-text">Amount Paid</th>
+  <th class="heading-text">Payment Date</th>
+  </tr>
+  <tr>
+    <td class="table-text">FEB 2021</td>
+  <td class="table-text">332</td>
+  <td class="table-text">0</td>
+  <td class="table-text">18/02/2021</td>
+  </tr>
+  <tr>
+    <td class="table-text">FEB 2021</td>
+  <td class="table-text">332</td>
+  <td class="table-text">0</td>
+  <td class="table-text">18/02/2021</td>
+  </tr>
+  <tr>
+    <td class="table-text">FEB 2021</td>
+  <td class="table-text">332</td>
+  <td class="table-text">0</td>
+  <td class="table-text">18/02/2021</td>
+  </tr>
+  <tr>
+    <td class="table-text">FEB 2021</td>
+  <td class="table-text">332</td>
+  <td class="table-text">0</td>
+  <td class="table-text">18/02/2021</td>
+  </tr>
+  <tr>
+    <td class="table-text">FEB 2021</td>
+  <td class="table-text">332</td>
+  <td class="table-text">0</td>
+  <td class="table-text">18/02/2021</td>
+  </tr>
+  <tr>
+    <td class="table-text">FEB 2021</td>
+  <td class="table-text">332</td>
+  <td class="table-text">0</td>
+  <td class="table-text">18/02/2021</td>
+  </tr>
+  <tr>
+    <td class="table-text">FEB 2021</td>
+  <td class="table-text">332</td>
+  <td class="table-text">0</td>
+  <td class="table-text">18/02/2021</td>
+  </tr>
+  <tr>
+    <td class="table-text">FEB 2021</td>
+  <td class="table-text">332</td>
+  <td class="table-text">0</td>
+  <td class="table-text">18/02/2021</td>
+  </tr>
+  <tr>
+    <td class="table-text">FEB 2021</td>
+  <td class="table-text">332</td>
+  <td class="table-text">0</td>
+  <td class="table-text">18/02/2021</td>
+  </tr>
+  <tr>
+    <td class="table-text">FEB 2021</td>
+  <td class="table-text">332</td>
+  <td class="table-text">0</td>
+  <td class="table-text">18/02/2021</td>
+  </tr>
+  <tr>
+    <td class="table-text">FEB 2021</td>
+  <td class="table-text">332</td>
+  <td class="table-text">0</td>
+  <td class="table-text">18/02/2021</td>
+  </tr>
+  <tr>
+    <td class="table-text">FEB 2021</td>
+  <td class="table-text">332</td>
+  <td class="table-text">0</td>
+  <td class="table-text">18/02/2021</td>
+  </tr>
+  <tr>
+    <td class="table-text">FEB 2021</td>
+  <td class="table-text">332</td>
+  <td class="table-text">0</td>
+  <td class="table-text">18/02/2021</td>
+  </tr>
+  
+</tbody>
+</table>
+</div>  
+
+</body>
